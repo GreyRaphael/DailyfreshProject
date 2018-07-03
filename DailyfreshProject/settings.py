dc4172e2675a9da36f05b67943fd764206b75e24
HPGrey <grey@pku.edu.cn>
2018-07-03 20:06:09 +0800

    change database; copy files to static; startapp df_user

--- DailyfreshProject/settings.py
+++ DailyfreshProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DailyfreshProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DailyFresh',
+        'USER': 'root',
+        'PASSWORD': '13810455459',
+        'HOST': '222.29.69.242',
+        'PORT': '3306',
     }
 }
 
