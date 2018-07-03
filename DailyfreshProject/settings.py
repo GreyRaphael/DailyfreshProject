c7b42f25520d0b3e87d5aad2db85685d0c1c4561
HPGrey <grey@pku.edu.cn>
2018-07-03 20:40:51 +0800

    create templates for df_user;

--- DailyfreshProject/settings.py
+++ DailyfreshProject/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DailyFresh.apps.DailyfreshConfig',
-    'df_user',
+    'df_user.apps.DfUserConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
