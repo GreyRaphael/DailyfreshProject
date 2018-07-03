122e7f019919a9666eda3df8bca075de53a717b6
HPGrey <grey@pku.edu.cn>
2018-07-03 20:14:33 +0800

    create df_user model and migration;

--- df_user/models.py
+++ df_user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=30)
+    ureceiver = models.CharField(max_length=20)
+    uaddr = models.CharField(max_length=100)
+    uzipcode = models.CharField(max_length=6)
+    uphone = models.CharField(max_length=11)
