cbf9905de8234a18a4b88a896893875631bc6047
HPGrey <grey@pku.edu.cn>
2018-07-03 21:09:48 +0800

    finish register and register_handle.

--- df_user/models.py
+++ df_user/models.py
@@ -4,9 +4,12 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
+    # sha1 length=40
     upwd = models.CharField(max_length=40)
     uemail = models.CharField(max_length=30)
-    ureceiver = models.CharField(max_length=20)
-    uaddr = models.CharField(max_length=100)
-    uzipcode = models.CharField(max_length=6)
-    uphone = models.CharField(max_length=11)
+    # default是给python用的, 不是给数据库用的, 不需要migration
+    # blank, null是数据库层面的，需要migration
+    ureceiver = models.CharField(max_length=20, default='')
+    uaddr = models.CharField(max_length=100, default='')
+    uzipcode = models.CharField(max_length=6, default='')
+    uphone = models.CharField(max_length=11, default='')
--- df_user/views.py
+++ df_user/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+import hashlib
 
 
 # Create your views here.
 def register(request):
     return render(request, 'register.html')
+
+
+def register_handle(request):
+    # get the form
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('pwd')
+    ucpwd = post.get('cpwd')
+    uemail = post.get('email')
+
+    # 判断两个密码，虽然js已经判断过了
+    if upwd != ucpwd:
+        return redirect('/user/register')
+
+    # encrypt pwd
+    m = hashlib.sha1()
+    m.update(bytes(upwd, 'utf8'))
+    encrypt_pwd = m.hexdigest()
+
+    # create UserInfo object
+    user = UserInfo()
+    user.uname = uname
+    user.upwd = encrypt_pwd
+    user.uemail = uemail
+    user.save()
+
+    # register success to redirect /user/login
+    return redirect('/user/login')
