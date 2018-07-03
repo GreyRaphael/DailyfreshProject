6b42a6de95effc309627a13a95ba28e23aa55f5f
HPGrey <grey@pku.edu.cn>
2018-07-03 22:18:25 +0800

    finish register and login_handle.

--- df_user/urls.py
+++ df_user/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register),
-    path('register_handle/', views.register_handle)
+    path('register_handle/', views.register_handle),
+    path('login/', views.login),
+    path('login_handle/', views.login_handle),
+    path('info/', views.info)
 ]
--- df_user/views.py
+++ df_user/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import *
 import hashlib
+from django.http import JsonResponse, HttpResponseRedirect
 
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+    context = {'title': '注册'}
+    return render(request, 'register.html', context)
 
 
 def register_handle(request):
@@ -34,3 +36,68 @@ def register_handle(request):
 
     # register success to redirect /user/login
     return redirect('/user/login')
+
+
+def register_exist(request):
+    uname = request.GET.get('uname')
+    count = UserInfo.objects.filter(uname=uname).count()
+    return JsonResponse({'count': count})
+
+
+def login(request):
+    uname = request.COOKIES.get('uname', '')
+    # 后面三个是给login_handle用的
+    context = {'title': '天天生鲜-登录', 'error_name': 0, 'error_pwd': 0, 'uname': uname}
+    return render(request, 'login.html', context)
+
+
+def login_handle(request):
+    # get the form
+    post = request.POST
+    uname = post.get('username')
+    upwd = post.get('upwd')
+    cb_flag = post.get('cb_flag', 0)
+
+    users = UserInfo.objects.filter(uname=uname)
+    if len(users) == 1:
+        # compared encrypt pwd
+        m = hashlib.sha1()
+        m.update(bytes(upwd, 'utf8'))
+        encrypt_pwd = m.hexdigest()
+        if encrypt_pwd == users[0].upwd:
+            red = HttpResponseRedirect('/user/info/')
+            if cb_flag != 0:
+                red.set_cookie('uname', uname)
+            else:
+                red.set_cookie('uname', '', max_age=-1)
+            request.session['user_id'] = users[0].id
+            request.session['user_name'] = uname
+            return red
+        else:
+            context = {
+                'title': '用户登录',
+                'error_name': 0,
+                'error_pwd': 1,
+                'uname': uname,
+                'upwd': upwd,
+            }
+            return render(request, 'login.html', context)
+    else:
+        context = {
+            'title': '用户登录',
+            'error_name': 1,
+            'error_pwd': 0,
+            'uname': uname,
+            'upwd': upwd,
+        }
+        return render(request, 'login.html', context)
+
+
+def info(request):
+    user_email = UserInfo.objects.get(id=request.session['user_id']).uemail
+    context = {
+        'title': '用户中心',
+        'user_email': user_email,
+        'user_name': request.session['user_name']
+    }
+    return render(request, 'user_center_info.html', context)
